8a7841733858dc06d4c5f23e3ee530df9d1c93b6
Yamil <yamil_diaz@hotmail.com>
2026-05-19 17:30:32 -0300

    Initial commit

---
+++ CineCritique/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CineCritique.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
